92e45a9a8d7a00a8abfdcee38d7c20cd33948063
Pedro <pmd.ribeiro@campus.fct.unl.pt>
2021-10-25 22:04:55 +0100

    Adding substance to FeedClass and User

--- src/FeedClass.java
+++ src/FeedClass.java
@@ -1,2 +1,14 @@
+import java.util.*;
+
 public class FeedClass {
+    List<User> userCollection;
+    public FeedClass(){
+        userCollection= new ArrayList<>();
+    }
+    public void addUser(String nome,boolean premium){
+        if(premium)
+            userCollection.add(new PremiumUser(nome));
+        else
+            userCollection.add(new StandardUser(nome));
+    }
 }
