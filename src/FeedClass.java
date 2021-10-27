e4db50ddcb0c43ed77d557a92320027ecc93aac0
Yuliia <y.bila@campus.fct.unl.pt>
2021-10-27 18:14:49 +0100

    Test iterators

--- src/FeedClass.java
+++ src/FeedClass.java
@@ -36,4 +36,8 @@ public class FeedClass {
             }
         }
     }
+    
+    public Iterator<User> getUsers() {
+    	return userCollection.iterator();
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Iterator;
+
 public class Main {
     public static void main(String[] args) {
         FeedClass f1 = new FeedClass();
@@ -10,5 +12,20 @@ public class Main {
 
         f1.addNews("CR7 marca golo", true);
         f1.addNews("Neymar marca golo", false);
+        
+        Iterator<User> usersIt = f1.getUsers();
+        User user;
+        
+        while (usersIt.hasNext()) {
+        	user = usersIt.next();
+        	System.out.println(user.getNome());
+        	Iterator<String> newsIterator = user.newsIterator();
+        	String news;
+        	while (newsIterator.hasNext()) {
+        		news = newsIterator.next();
+        		System.out.println("---- " + news);
+        	}	
+        }
+        
     }
 }
